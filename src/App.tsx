04382aec1ab2224b8108779b52ed06413ab7ea54
Valdir Coxev <valdircoxevdev@gmail.com>
2021-09-06 11:26:41 -0400

    chore: added @material-ui/core package

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <h1>Chuck Norris Jokes</h1>
+    <>
+      <h1>Chuck Norris Jokes</h1>
+      <Button variant="contained" color="primary">
+        Jokes
+      </Button>
+    </>
   );
 }
 
